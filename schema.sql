9b2d2ad376799674e3d401ae8977fd91906befee
Steven Liou <stevenliou@gmail.com>
2021-09-30 22:21:28 -0400

    Initialized the database

---
+++ schema.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE todolists (
+  id serial PRIMARY KEY,
+  title varchar(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE todos (
+  id serial PRIMARY KEY,
+  title varchar(100) NOT NULL,
+  done BOOLEAN NOT NULL DEFAULT FALSE,
+  todolist_id INTEGER NOT NULL
+  REFERENCES todolists(id)
+  ON DELETE CASCADE
+);
